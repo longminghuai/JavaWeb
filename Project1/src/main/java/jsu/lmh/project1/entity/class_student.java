c521d81d82acdfa21a22ad89a5d8c17d3828a79c
longminghuai <127707783+longminghuai@users.noreply.github.com>
2024-06-06 22:30:12 +0800

    Create class_student.java

---
+++ Project1/src/main/java/jsu/lmh/project1/entity/class_student.java
@@ -0,0 +1,15 @@
+package jsu.lmh.project1.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class class_student {
+    public Long id;
+    public Long student_id;
+    public Long class_id;
+}
